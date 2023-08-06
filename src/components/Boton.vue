3c3fcbcd241da7f368bda4ff448e359e75523560
Klefur <joaquinalberto55@gmail.com>
2023-08-06 17:40:22 -0400

    chore: diseño para pantalla completa y reubicacion de archivos

---
+++ src/components/Boton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button :onclick="onClick" class="rounded-lg bg-usach-terra-700 hover:bg-usach-terra-800 text-lg text-white my-2 drop-shadow p-2">
+        <a className="font-usach-helvetica-medium px-max" :href="href">
+            {{ label }}
+        </a>
+    </button>
+</template>
+
+<script setup>
+    const props = defineProps({
+        href: {
+            type: String,
+            required: false
+        },
+        label: String,
+        onClick:  {
+            type: Function,
+            required: false
+        }
+    })
+</script>
---
+++ src/layout/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+    <Header title="Recursos Virtuales para el Lenguaje"></Header>
+    <main class="bg-usach-aqua-1000 flex flex-auto py-10 justify-center align-middle">
+        <router-view></router-view>
+    </main>
+    <Footer></Footer>
+</template>
+
+<script setup>
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+</script>

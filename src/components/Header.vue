fb13f3a3e4650409f4e12082e9efd11bbb3f3215
Skyrdow <lucas.mesias@usach.cl>
2023-08-15 22:38:41 -0400

    fix: header y enlace
    arreglado tamaño del logo usach y uso del componente enlace

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,25 @@
 <template>
     <header className="bg-usach-aqua-900 flex items-center p-4">
         
-        <button className="min-h-16 h-16 mr-2">
+        <button className="flex-none">
             <router-link to="/">
-                <img src="/Usach SB.png" alt="Logo Usach" className=" w-full h-full object-contain"/>
+                <img src="/Usach SB.png" alt="Logo Usach" className="logo-sb hidden lg:block"/>
+                <img src="/Usach PB.png" alt="Logo Usach" className="logo-pb lg:hidden block"/>
             </router-link>
         </button>
         <div class="flex-grow text-center">
-            <h1 className="lg:text-5xl sm:text-3xl font-usach-bebas-title text-white ">Recursos Virtuales para el aprendizaje del Lenguaje</h1>
+            <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-usach-bebas-title text-white ">Recursos Virtuales para el aprendizaje del Lenguaje</h1>
         </div>
     </header>
 </template>
+
+<style>
+.logo-pb{
+    height: 170px;
+    width: 170px;
+}
+.logo-sb{
+    height: 170px;
+    width: 538px;
+}
+</style>

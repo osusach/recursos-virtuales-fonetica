f5ed83774459030777cf0eb4df45d53f9477f20e
Skyrdow <lucas.mesias@usach.cl>
2023-08-19 12:53:00 -0400

    fix: axios get funcionando
    hay que ver si el suspense funciona realmente o lo sacamos

--- src/views/Cat-acentual.vue
+++ src/views/Cat-acentual.vue
@@ -1,52 +1,62 @@
 <template>
-    <div class="grid items-center">
-        <div class="flex flex-col gap-4">
-            <div class="flex flex-row h-14 rounded-lg bg-usach-ultra-900 align-middle items-center">
-                <div class="flex static bg-usach-ultra-300 h-3 w-full rounded-xl mr-1 ml-6 justify-center items-center font-usach-bebas-title">
-                    <button v-for="num in Array.from({ length: cantPregs }, (v, i) => i)" @click="changeQuestion(num + 1)"
-                    class="mx-2 rounded-full h-7 w-10 text-lg" :class="{
-                        'bg-usach-ultra-300 font-usach-bebas-body': store.respuestas[num] === undefined,
-                        'bg-usach-terra-700 text-slate-100': store.respuestas[num] !== undefined
-                    }">
-                        {{ num + 1 }}
-                    </button>
-                </div>
-                
-                <button class=" font-usach-bebas-title mx-3 bg-usach-terra-700 rounded-lg p-1 text-white text-lg" @click="endQuiz">
-                    {{ Fin }}
-                </button>
-            </div>
-            <div class=" flex flex-col rounded-lg bg-usach-ultra-900 p-10 text-center font-usach-helvetica-body text-white items-center">
-                <div class=" font-usach-helvetica-bold text-lg">Pregunta {{ i + '/' + cantPregs }}</div>
-                <div class="bg-usach-ultra-600 text-7xl p-3 rounded-lg my-5 font-usach-helvetica-bold">
-                    <p class=" pt-4">{{ palabra }}</p>
-                </div>
-                <p class="text-center text-xl">¿A qué categoría acentual pertenece esta palabra?</p>
-                <div class="flex flex-row p-5 rounded-lg bg-usach-ultra-600 mt-3 mb-7">
-                    <div v-for="opcion in opciones" class="flex space-x-2">
-                        <input type="radio" :id="'answer-' + opcion" :value="opcion" v-model="respuesta" class="hidden" />
-                        <label :for="'answer-' + opcion"
-                            class="px-4 py-2 border rounded-lg cursor-pointer transition-all duration-200 ease-in-out pt-3"
-                            :class="{
-                                ' bg-usach-aqua-800 text-white': respuesta === opcion,
-                                'bg-gray-200 text-gray-700 hover:bg-gray-300': respuesta !== opcion
+    <Suspense>
+
+        <template #default>
+            <div class="grid items-center">
+                <div class="flex flex-col gap-4">
+                    <div class="flex flex-row h-14 rounded-lg bg-usach-ultra-900 align-middle items-center">
+                        <div class="flex static bg-usach-ultra-300 h-3 w-full rounded-xl mr-1 ml-6 justify-center items-center font-usach-bebas-title">
+                            <button v-for="num in Array.from({ length: cantPregs }, (v, i) => i)" @click="changeQuestion(num + 1)"
+                            class="mx-2 rounded-full h-7 w-10 text-lg" :class="{
+                                'bg-usach-ultra-300 font-usach-bebas-body': store.respuestas[num] === undefined,
+                                'bg-usach-terra-700 text-slate-100': store.respuestas[num] !== undefined
                             }">
-                            {{ opcion }}
-                        </label>
+                                {{ num + 1 }}
+                            </button>
+                        </div>
+                        
+                        <button class=" font-usach-bebas-title mx-3 bg-usach-terra-700 rounded-lg p-1 text-white text-lg" @click="endQuiz">
+                            {{ Fin }}
+                        </button>
+                    </div>
+                    <div class=" flex flex-col rounded-lg bg-usach-ultra-900 p-10 text-center font-usach-helvetica-body text-white items-center">
+                        <div class=" font-usach-helvetica-bold text-lg">Pregunta {{ i + '/' + cantPregs }}</div>
+                        <div class="bg-usach-ultra-600 text-7xl p-3 rounded-lg my-5 font-usach-helvetica-bold">
+                            <p class=" pt-4">{{ palabra }}</p>
+                        </div>
+                        <p class="text-center text-xl">¿A qué categoría acentual pertenece esta palabra?</p>
+                        <div class="flex flex-row p-5 rounded-lg bg-usach-ultra-600 mt-3 mb-7">
+                            <div v-for="opcion in opciones" class="flex space-x-2">
+                                <input type="radio" :id="'answer-' + opcion" :value="opcion" v-model="respuesta" class="hidden" />
+                                <label :for="'answer-' + opcion"
+                                    class="px-4 py-2 border rounded-lg cursor-pointer transition-all duration-200 ease-in-out pt-3"
+                                    :class="{
+                                        ' bg-usach-aqua-800 text-white': respuesta === opcion,
+                                        'bg-gray-200 text-gray-700 hover:bg-gray-300': respuesta !== opcion
+                                    }">
+                                    {{ opcion }}
+                                </label>
+                            </div>
+                        </div>
+        
+                        <div class="flex flex-row justify-center gap-4 font-usach-bebas-body">
+                            <button v-if="i > 1" @click="prevQuestion" class="bg-usach-aqua-800 rounded-lg p-2">Atrás</button>
+                            <button @click="nextQuestion" class="bg-usach-aqua-800 rounded-lg p-2"> {{ nextText }} </button>
+                        </div>
                     </div>
                 </div>
-
-                <div class="flex flex-row justify-center gap-4 font-usach-bebas-body">
-                    <button v-if="i > 1" @click="prevQuestion" class="bg-usach-aqua-800 rounded-lg p-2">Atrás</button>
-                    <button @click="nextQuestion" class="bg-usach-aqua-800 rounded-lg p-2"> {{ nextText }} </button>
-                </div>
             </div>
-        </div>
-    </div>
+        </template>
+        <template #fallback>
+            <div>
+                loading
+            </div>
+        </template>
+    </Suspense>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { store } from '../store.js'
 import axios from 'axios'
@@ -65,15 +75,11 @@ const i = ref(1)
 let palabra = ref('')
 let opciones = ref([])
 
-const response = await axios.get('https://pindaro.pindarousach.workers.dev/acentual/start/1')
-const apiResponse = response.data
-console.log('apiResponse')
-console.log(apiResponse)
-let respuesta = ref('')
-const nextText = ref('Siguiente')
-
+let apiResponse = null
 
 const changeQuestionApi = () => {
+    if (apiResponse === null)
+        { return }
     console.log('i.value')
     console.log(i.value)
     let question = apiResponse[i.value - 1]
@@ -84,6 +90,22 @@ const changeQuestionApi = () => {
     console.log(palabra.value)
     console.log(opciones.value)
 }
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('https://pindaro.pindarousach.workers.dev/acentual/start/1')  
+    apiResponse = response.data
+    changeQuestionApi()
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+})
+
+console.log('apiResponse')
+console.log(apiResponse)
+let respuesta = ref('')
+const nextText = ref('Siguiente')
+
 
 const saveAnswer = () => {
     // no borrar respuesta al navegar

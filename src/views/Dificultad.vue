fef9f43a3ec09e23abfdb4a847163345d60734de
Nicolás Aguilera <nicolas.aguilera.g@usach.cl>
2023-08-19 20:55:39 -0400

    fix: cargando y vista dificultad

--- src/views/Dificultad.vue
+++ src/views/Dificultad.vue
@@ -1,25 +1,21 @@
 <template>
-    <form class="m-auto grid xl:grid-cols-1 gap-10 p-10 min-w-[35%]">
-      	<div class="text-5xl h-fit text-white text-center font-usach-helvetica-body bg-usach-aqua-800 rounded-lg">
-		  	<div class="flex flex-col p-10 justify-center items-center">
-				<p>Juego Actual: {{ nombreJuego }}</p>
-			  	<h1 class="text-3xl mb-5 font-usach-bebas-title">
+		<div class=" m-auto flex flex-col p-10 justify-center items-center text-center font-usach-helvetica-body bg-usach-aqua-800 rounded-lg text-white">
+				<p class=" mb-9 text-2xl">Juego Actual: {{ nombreJuego }}</p>
+			  	<h1 class="text-3xl mb-1 font-usach-bebas-title">
 				  	Selecciona una dificultad
 				</h1>
 				<div class="flex flex-col w-full">
 					<router-link :to='ruta'>
-						<Boton label="Básico" type="submit" @click="setDificultad(1)" class="bg-usach-daisy-700 hover:bg-usach-daisy-900 text-xl"></Boton>
+						<Boton label="Básico" type="submit" @click="setDificultad(1)" class="bg-usach-daisy-700 hover:bg-usach-daisy-900 text-xl w-64"></Boton>
 					</router-link>
 					<router-link :to='ruta'>
-						<Boton label="Medio" type="submit" @click="setDificultad(2)" class="bg-usach-terra-700 hover:bg-usach-terra-900 text-xl"></Boton>
+						<Boton label="Medio" type="submit" @click="setDificultad(2)" class="bg-usach-terra-700 hover:bg-usach-terra-900 text-xl w-64"></Boton>
 					</router-link>
 					<router-link :to='ruta'>
-						<Boton label="Avanzado" type="submit" @click="setDificultad(3)" class="bg-usach-ultra-700 hover:bg-usach-ultra-900 text-xl"></Boton>
+						<Boton label="Avanzado" type="submit" @click="setDificultad(3)" class="bg-usach-ultra-700 hover:bg-usach-ultra-900 text-xl w-64"></Boton>
 					</router-link>
 				</div>
 			</div>
-      	</div>
-    </form>
   </template>
   
 <script setup>

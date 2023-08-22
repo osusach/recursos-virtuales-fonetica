2b81b6212e3fecdeffde920205ee084b59b9ef83
Skyrdow <lucas.mesias@usach.cl>
2023-08-21 22:39:01 -0400

    feat: cambios a leaderboard

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="rounded-lg bg-usach-daisy-700 p-5">
-        <label id="title" class="block font-bold mb-3">Tabla de Puntajes</label>
+        <label id="title" class="block font-bold mb-3">Tabla de Puntuaciones {{ label }}</label>
         <div class="max-h-80 overflow-y-auto rounded-lg border border-gray-300"> 
             <table id="table" class="table-auto min-w-full border-collapse bg-white w-full">
                 <thead>
                     <tr>
-                        <th class="border border-gray-300 px-4 py-2"> Nombre </th>
-                        <th class="border border-gray-300 px-4 py-2"> Puntaje </th>
+                        <th> Nombre </th>
+                        <th> Puntaje </th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr v-for="(item, index) in items" :key="index">
-                        <td class="border border-gray-300 px-4 py-2">{{ item.nombre }}</td>
-                        <td class="border border-gray-300 px-4 py-2">{{ item.score }}</td>
+                        <td>{{ item.nombre }}</td>
+                        <td>{{ item.score }}</td>
                     </tr>
                 </tbody>
             </table>
@@ -25,7 +25,14 @@
 import { ref } from 'vue'
 let items = ref([])
 
+const props = defineProps({
+        label: String,
+		dbHttp: String
+    })
+
+
 const getLeaderboard = (tipo) => {
+	// axios.get(dbHttp)
     return [ 
         { nombre: "schlawg", score: 25}, 
         { nombre: "juaquin", score: 2},
@@ -58,5 +65,20 @@ items = getLeaderboard(1)
     text-align: center;
     align-items: center;
     justify-content: center;
+}
+th {
+	position: sticky;
+	top: 0;
+    --tw-bg-opacity: 1;
+    background-color: rgb(199 221 244 / var(--tw-bg-opacity));
+}
+th, td {
+	padding-top: 0.5rem/* 8px */;
+    padding-bottom: 0.5rem/* 8px */;
+	padding-left: 1rem/* 16px */;
+    padding-right: 1rem/* 16px */;
+	--tw-border-opacity: 1;
+    border-color: rgb(209 213 219 / var(--tw-border-opacity));
+	border-width: 1px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
-    <Slide></Slide>
-    <Leaderboard></Leaderboard>
+    <div class="flex flex-col gap-10">
+        <Slide></Slide>
+        <div class="grid grid-flow-col gap-5">
+            <Leaderboard label="Píndaro"></Leaderboard>
+            <Leaderboard></Leaderboard>
+            <Leaderboard></Leaderboard>
+        </div>
+    </div>
 </template>
 
 <script setup>

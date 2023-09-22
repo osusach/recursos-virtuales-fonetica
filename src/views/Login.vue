74e68a9207ca5a8b8eedfdddea67820f72fbd86f
Skyrdow <lucas.mesias@usach.cl>
2023-09-21 23:46:55 -0300

    fix: router-view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,22 @@
 <template>
-  <form class="m-auto grid xl:grid-cols-1 gap-10 p-10 min-w-[35%]">
-    <div class="flex flex-col text-5xl h-fit p-10 text-white text-center font-usach-helvetica-body bg-usach-aqua-800 rounded-lg [&>button]:">
-      <h1 class="text-3xl mb-5 font-usach-bebas-title">
-        ¡Bienvenido!
-      </h1>
-      <Input Label="Correo" forLabel="email" type="text" placeholder="pindaro@usach.cl"/>
-      <Input Label="Contraseña" forLabel="password" type="password" placeholder="Contraseña"/>
-      <Boton label="Iniciar Sesión" href="/" @click="loginFunc()" class="bg-usach-daisy-700 hover:bg-usach-daisy-900 text-xl"/>
-      <div className="font-usach-helvetica-medium rounded-lg  p-4 text-lg">
-        ¿No tienes cuenta?<Enlaces to="/register">Regístrate</Enlaces>
-      </div>
-      <div className="font-usach-helvetica-medium rounded-lg  px-6 text-lg">
-        <label>¿Prefieres no registrarte aún?</label>
-      </div>
-      <Boton label="Invitado" @click="guestFunc()" class="bg-usach-ultra-700 hover:bg-usach-ultra-900 text-xl"/>
-    </div>
-  </form>
+	<form class="m-auto grid xl:grid-cols-1 gap-10 p-10 min-w-[35%]">
+		<div class="flex flex-col text-5xl h-fit p-10 text-white text-center font-usach-helvetica-body bg-usach-aqua-800 rounded-lg [&>button]:">
+		<h1 class="text-3xl mb-5 font-usach-bebas-title">
+			¡Bienvenido!
+		</h1>
+		<Input Label="Correo" forLabel="email" type="text" placeholder="pindaro@usach.cl"/>
+		<Input Label="Contraseña" forLabel="password" type="password" placeholder="Contraseña"/>
+		<Boton label="Iniciar Sesión" href="/" @click="loginFunc()" class="bg-usach-daisy-700 hover:bg-usach-daisy-900 text-xl"/>
+		<div className="font-usach-helvetica-medium rounded-lg  p-4 text-lg">
+			¿No tienes cuenta?
+			<router-link to="/register"> <b>Regístrate</b> </router-link>
+		</div>
+		<div className="font-usach-helvetica-medium rounded-lg  px-6 text-lg">
+			<label>¿Prefieres no registrarte aún?</label>
+		</div>
+		<Boton label="Invitado" @click="guestFunc()" class="bg-usach-ultra-700 hover:bg-usach-ultra-900 text-xl"/>
+		</div>
+	</form>
 </template>
 
 <script setup>

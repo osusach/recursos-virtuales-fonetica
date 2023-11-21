e044c1bd6db523e8d2a61569acf5b8340fc40cc5
Skyrdow <lucas.mesias@usach.cl>
2023-11-20 21:24:04 -0300

    fix: emails

--- src/views/Cat-acentual.vue
+++ src/views/Cat-acentual.vue
@@ -12,14 +12,14 @@
 import Juego from "../components/Juego.vue";
 
 function acentualQuestion(question) {
-	let palabra = question.word;
-	let frase = question.phrase.split(palabra);
-	return `<div class="bg-usach-ultra-600 p-3 rounded-lg my-5">
-                <p class="cat-text max-w-[20ch] font-usach-helvetica-bold">
-                    ${frase[0]}
-                    <b class="bg-usach-cloudy-900 rounded-md px-2 py-1">${palabra}</b>
-                    ${frase[1]}
-                </p>
-            </div>`;
+	let pos = question.word_pos + 1;
+	let frase = question.phrase.split(" ");
+	return `<div class="bg-usach-ultra-600 p-3 rounded-lg my-5">` +
+				`<p class="cat-text max-w-[20ch] font-usach-helvetica-bold">` +
+                    frase.slice(0, pos - 1).join(" ") + " " +
+                    `<b class="bg-usach-cloudy-900 rounded-md px-2 py-1">${frase[pos - 1]}</b>` + " " +
+                    frase.slice(pos).join(" ") +
+                `</p>` +
+            `</div>`;
 }
 </script>

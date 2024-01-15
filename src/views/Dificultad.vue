d6b343354024883786338562f5d3aad543d26ac3
Skyrdow <lucas.mesias@usach.cl>
2024-01-15 20:25:07 -0300

    fix: responsive dificultad

--- src/views/Dificultad.vue
+++ src/views/Dificultad.vue
@@ -1,33 +1,35 @@
 <template>
-	<div class="flex flex-col p-7 bg-usach-aqua-800 font-usach-helvetica-body text-white mr-10 rounded-lg">
-		<h1 class="">
-			Quieres guardar tu puntaje en el leaderboard?
-		</h1>
-		<a href="/login" class="font-usach-helvetica-bold hover:underline">
-			Inicia Sesión
-		</a>
-	</div>
-
-	<div
-		class="flex flex-col p-7 justify-center items-center text-center text-lg font-usach-helvetica-body bg-usach-aqua-800 rounded-lg text-white">
-		<div class="flex flex-row gap-2 mb-5">
-			<p>Juego actual:</p>
-			<p class="font-usach-helvetica-bold">{{ nombreJuego }}</p>
+	<div class="flex flex-col md:flex-row gap-5 items-center">
+		<div class="flex flex-col p-7 bg-usach-aqua-800 font-usach-helvetica-body text-white rounded-lg h-fit">
+			<h1 class="">
+				Quieres guardar tu puntaje en el leaderboard?
+			</h1>
+			<a href="/login" class="font-usach-helvetica-bold hover:underline">
+				Inicia Sesión
+			</a>
 		</div>
-		<h1 class="text-xl mb-1">Selecciona una dificultad</h1>
-		<div class="flex flex-col w-full">
-			<router-link :to="ruta">
-				<Boton label="Básico" type="submit" @click="setDificultad(0)"
-					class="bg-usach-daisy-700 hover:bg-usach-daisy-900 text-xl w-64"></Boton>
-			</router-link>
-			<router-link :to="ruta">
-				<Boton label="Medio" type="submit" @click="setDificultad(1)"
-					class="bg-usach-terra-700 hover:bg-usach-terra-900 text-xl w-64"></Boton>
-			</router-link>
-			<router-link :to="ruta">
-				<Boton label="Avanzado" type="submit" @click="setDificultad(2)"
-					class="bg-usach-ultra-700 hover:bg-usach-ultra-900 text-xl w-64"></Boton>
-			</router-link>
+	
+		<div
+			class="flex flex-col p-7 justify-center items-center text-center text-lg font-usach-helvetica-body bg-usach-aqua-800 rounded-lg text-white">
+			<div class="flex flex-row gap-2 mb-5">
+				<p>Juego actual:</p>
+				<p class="font-usach-helvetica-bold">{{ nombreJuego }}</p>
+			</div>
+			<h1 class="text-xl mb-1">Selecciona una dificultad</h1>
+			<div class="flex flex-col w-full">
+				<router-link :to="ruta">
+					<Boton label="Básico" type="submit" @click="setDificultad(0)"
+						class="bg-usach-daisy-700 hover:bg-usach-daisy-900 text-xl w-64"></Boton>
+				</router-link>
+				<router-link :to="ruta">
+					<Boton label="Medio" type="submit" @click="setDificultad(1)"
+						class="bg-usach-terra-700 hover:bg-usach-terra-900 text-xl w-64"></Boton>
+				</router-link>
+				<router-link :to="ruta">
+					<Boton label="Avanzado" type="submit" @click="setDificultad(2)"
+						class="bg-usach-ultra-700 hover:bg-usach-ultra-900 text-xl w-64"></Boton>
+				</router-link>
+			</div>
 		</div>
 	</div>
 </template>
